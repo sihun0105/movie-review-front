c0cee4e6097cbc98a099e0b7ef46feb00f8330ba
KIMSIHUN <tlgns14@nate.com>
2023-12-20 10:31:02 +0900

    🎨  footer 추가

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -1,11 +1,22 @@
 import { FunctionComponent } from 'react'
+import { Mail, Github } from 'lucide-react'
 interface FooterProps {}
 const Footer: FunctionComponent<FooterProps> = () => {
   return (
     <section>
-      <div className="mb-[15px] text-center text-xs font-normal text-app-gray-004">
-        <p>email : tlgns14@nate.com</p>
-        <p>github : https://github.com/sihun0105</p>
+      <div className="mb-[15px] flex flex-col items-center space-y-4 text-xs font-normal text-app-gray-007">
+        <a href="mailto:tlgns14@nate.com" className="flex flex-row space-x-2">
+          <Mail />
+          <p>email :tlgns14@nate.com</p>
+        </a>
+        <a
+          href="https://github.com/sihun0105"
+          target="_blank"
+          className="flex flex-row space-x-2"
+        >
+          <Github />
+          <p>github : https://github.com/sihun0105</p>
+        </a>
       </div>
     </section>
   )
