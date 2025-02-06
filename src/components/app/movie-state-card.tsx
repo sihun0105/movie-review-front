289a3950b5b0003e9e9bc750ddeea54098a1a3aa
KIMSIHUN <tlgns14@nate.com>
2025-02-06 22:49:45 +0900

    💄

--- src/components/app/movie-state-card.tsx
+++ src/components/app/movie-state-card.tsx
@@ -11,15 +11,15 @@ interface MovieCardProps {
 const MovieCard: FunctionComponent<MovieCardProps> = ({ data: movie }) => {
   return (
     <Link href={`movie/${movie.id}`}>
-      <div className="stats relative w-full overflow-visible rounded-lg bg-white shadow-lg transition-transform duration-300 hover:scale-105 dark:bg-gray-800">
+      <div className="stats relative w-full overflow-visible rounded-lg bg-white shadow-lg transition-transform duration-300 hover:scale-105 ">
         {movie.rank === 1 && (
           <FaCrown className="absolute left-[-10px] top-[-10px] z-10 text-3xl text-yellow-500 transition-transform duration-300" />
         )}
         <div className="stat p-4">
           <section className="relative aspect-square w-full overflow-hidden rounded-t-lg">
             {movie.poster === '' && (
-              <div className="absolute inset-0 flex items-center justify-center bg-gray-200 dark:bg-gray-600">
-                <p className="text-gray-500 dark:text-gray-300">No Image</p>
+              <div className="absolute inset-0 flex items-center justify-center bg-gray-200 ">
+                <p className="text-gray-500 ">No Image</p>
               </div>
             )}
             <Image
@@ -28,26 +28,21 @@ const MovieCard: FunctionComponent<MovieCardProps> = ({ data: movie }) => {
               blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNk+M9QDwADhgGAWjR9awAAAABJRU5ErkJggg=="
               placeholder="blur"
               fill
-              className="object-cover"
             />
-            <div className="absolute bottom-2 left-2 text-5xl font-bold text-gray-300 dark:text-gray-300">
+            <div className="absolute bottom-2 left-2 text-5xl font-bold text-gray-300">
               {movie.rank}
             </div>
           </section>
-          <section className="mt-4 text-black dark:text-white">
-            <div className="stat-title whitespace-normal text-lg font-semibold dark:text-white">
+          <section className="mt-4 text-black ">
+            <div className="stat-title whitespace-normal text-lg font-semibold ">
               {movie.title}
             </div>
-            <div className="stat-value text-xl font-bold dark:text-white">
+            <div className="stat-value text-xl font-bold ">
               {movie.audience.toLocaleString()}
             </div>
-            <div className="stat-desc mt-2 dark:text-white">
-              장르: {movie.genre}
-            </div>
-            <div className="stat-desc dark:text-white">
-              감독: {movie.director}
-            </div>
-            <div className="stat-desc dark:text-white">
+            <div className="stat-desc mt-2 ">장르: {movie.genre}</div>
+            <div className="stat-desc ">감독: {movie.director}</div>
+            <div className="stat-desc ">
               업데이트 날짜: {new Date(movie.updatedAt).toLocaleDateString()}
             </div>
           </section>
