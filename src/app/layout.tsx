7f6faae8b40001a8ee103bf369a1f80b39e43982
KIMSIHUN <tlgns14@nate.com>
2025-08-02 14:40:51 +0900

    🎨 google analytics적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { SessionProvider } from '@/providers/session-provider'
 import '@/styles/globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
+import Script from 'next/script'
 
 const roboto = Roboto({
   weight: ['400', '700'],
@@ -126,6 +127,7 @@ export default function RootLayout({
           name="google-site-verification"
           content="4StxYcl8XHxRM28X7KZbRNrE7fjOjFH5oM8XBKk9YkM"
         />
+
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
@@ -180,6 +182,31 @@ export default function RootLayout({
           roboto.className,
         )}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K2TN4MLS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K2TN4MLS');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         <SessionProvider>
           <AppThemeProvider>
             <MessageModalContextProvider>
