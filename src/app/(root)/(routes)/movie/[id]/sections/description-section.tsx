4cecc75b811c071d078cbb3bca4317cf5c24972c
KIMSIHUN <tlgns14@nate.com>
2026-06-03 18:08:00 +0900

    🐛 영화 상세 VOD 버튼 동작 연결

--- src/app/(root)/(routes)/movie/[id]/sections/description-section.tsx
+++ src/app/(root)/(routes)/movie/[id]/sections/description-section.tsx
@@ -5,6 +5,7 @@ import { FunctionComponent } from 'react'
 import MovieCarousel from '../components/movie-carousel'
 import MovieVodModal from '../components/movie-vod-modal'
 import { useGetMovieDetail } from '../hooks/use-get-movie-detail'
+import { useVodModalContext } from '../hooks/use-vod-modal-context'
 
 interface DescriptionSectionProps {
   id: string
@@ -14,6 +15,7 @@ const DescriptionSection: FunctionComponent<DescriptionSectionProps> = ({
   id,
 }) => {
   const { data, isLoading, error } = useGetMovieDetail(id)
+  const { setOpen, setSrc, setTitle } = useVodModalContext()
 
   if (isLoading)
     return <p className="text-center text-muted-foreground">로딩 중...</p>
@@ -22,10 +24,19 @@ const DescriptionSection: FunctionComponent<DescriptionSectionProps> = ({
       <p className="text-center text-primary">데이터를 불러오지 못했습니다.</p>
     )
 
+  const firstVod = data.vods?.[0]
+  const handleVodClick = firstVod
+    ? () => {
+        setSrc(firstVod.vodUrl)
+        setTitle(data.title)
+        setOpen(true)
+      }
+    : undefined
+
   return (
     <>
       <MovieVodModal />
-      <DmMovieDetail movie={data} />
+      <DmMovieDetail movie={data} onVodClick={handleVodClick} />
       {data.vods && <MovieCarousel data={data.vods} title={data.title} />}
     </>
   )
