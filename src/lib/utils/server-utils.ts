662262e041a135cf1c137183e6c92e5b84da4117
KIMSIHUN <tlgns14@nate.com>
2026-09-05 22:37:06 +0900

    refactor: 미사용 UI 제거와 검증 흐름 정리

--- src/lib/utils/server-utils.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only'
-import { getServerSession } from 'next-auth'
-import { authOptions } from '@/app/api/auth/[...nextauth]/next-option'
-
-export async function getCurrentUser() {
-  const session = await getServerSession(authOptions)
-  return session?.user ?? null
-}
